db57db8bbf6e7b5f42766e16527b673280f5e627
Aydin-Firoozshahian <aydin26f@gmail.com>
2026-02-13 15:27:40 -0800

    changed motor ids to match motor map

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -8,14 +8,14 @@ public final class Ports {
     public static final CANBus kCANivoreCANBus = new CANBus("main");
 
     // Talon FX IDs
-    public static final int kIntakePivot = 10;
-    public static final int kIntakeRollers = 11;
+    public static final int kIntakePivot = 13;
+    public static final int kIntakeRollers = 14;
     public static final int kFloor = 12;
-    public static final int kFeeder = 13;
-    public static final int kShooterLeft = 14;
-    public static final int kShooterMiddle = 15;
-    public static final int kShooterRight = 16;
-    public static final int kHanger = 18;
+    public static final int kFeeder = 15;
+    public static final int kShooterLeft = 11;
+    public static final int kShooterMiddle = 10;
+    public static final int kShooterRight = 9;
+    public static final int kHanger = 16;
 
     // PWM Ports
     public static final int kHoodLeftServo = 3;
