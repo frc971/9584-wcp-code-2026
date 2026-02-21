f3cbda3f51f8190e31df4685fb9f46c92c9a86ce
Aydin-Firoozshahian <aydin26f@gmail.com>
2026-02-20 16:10:39 -0800

    change hood max speed and position

--- src/main/java/frc/robot/subsystems/Hood.java
+++ src/main/java/frc/robot/subsystems/Hood.java
@@ -20,9 +20,9 @@ import frc.robot.Ports;
 
 public class Hood extends SubsystemBase {
     private static final Distance kServoLength = Millimeters.of(100);
-    private static final LinearVelocity kMaxServoSpeed = Millimeters.of(20).per(Second);
+    private static final LinearVelocity kMaxServoSpeed = Millimeters.of(40).per(Second);
     private static final double kMinPosition = 0.01;
-    private static final double kMaxPosition = 0.77;
+    private static final double kMaxPosition = 0.9;
     private static final double kPositionTolerance = 0.01;
 
     private final Servo servo;
