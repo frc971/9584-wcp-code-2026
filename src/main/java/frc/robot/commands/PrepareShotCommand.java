cdc7ba2146f20433afb6723c2b9476106ef13f2e
Aydin-Firoozshahian <aydin26f@gmail.com>
2026-03-14 14:00:55 -0700

    change shot table distances to match kohl's field setpoints

--- src/main/java/frc/robot/commands/PrepareShotCommand.java
+++ src/main/java/frc/robot/commands/PrepareShotCommand.java
@@ -42,8 +42,9 @@ public class PrepareShotCommand extends Command {
 
      static {
          distanceToShotMap.put(Inches.of(52.0), new Shot(3080, 0.35)); //increased by 10% - if we can test thats good
-         distanceToShotMap.put(Inches.of(114.4), new Shot(3605, 0.60));
-         distanceToShotMap.put(Inches.of(165.5), new Shot(4015, 0.78));
+         distanceToShotMap.put(Inches.of(114.0), new Shot(3605, 0.60));
+         distanceToShotMap.put(Inches.of(139.0), new Shot(4015, 0.78));
+         distanceToShotMap.put(Inches.of(156.0), new Shot(4015, 0.78));
     }
 
     private final Shooter shooter;
